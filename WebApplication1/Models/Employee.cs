e6bf3b9f7eaa5fb153acbc872eb14ddb4589f1d3
Eya <eya@admin.com>
2025-02-28 11:56:52 +0100

    Ajoutez des fichiers projet.

---
+++ WebApplication1/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(10,ErrorMessage ="Taille Max 10")]
+        public string Name { get; set; }
+
+        [Required] 
+        public string Departement { get; set; }
+
+        [Range(200,5000)]
+        public int Salary { get; set; }
+    }
+}
